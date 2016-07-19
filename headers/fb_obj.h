1a887d5c063e4594853e1eebd2cdcd9ea35ff45e
Dmytro Prokofiev <prokofjev.d@gmail.com>
2016-07-19 19:14:51 +0300

    cubes are clickable

--- headers/fb_obj.h
+++ headers/fb_obj.h
@@ -4,13 +4,18 @@
 #include <QtQuick/QQuickFramebufferObject>
 #include "headers/logorenderer.h"
 
+class Fbo_renderer;
+
 class Fb_obj : public QQuickFramebufferObject
 {
   Q_OBJECT
 public:
+  ~Fb_obj();
   Renderer *createRenderer() const;
 
   Q_INVOKABLE void pressEvent();
+
+  mutable Fbo_renderer *renderer;
 };
 
 class Fbo_renderer : public QQuickFramebufferObject::Renderer
@@ -19,6 +24,7 @@ public:
   Fbo_renderer();
   void render();
   QOpenGLFramebufferObject* createFramebufferObject(const QSize &size);
+  void scale(const double sc);
 
 private:
   LogoRenderer logo;
